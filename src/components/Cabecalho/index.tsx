19abdbee96984594fb0491b2977632e97d92c981
Marcio santos <marciosantos40graus@gmail.com>
2026-05-19 15:38:32 -0300

    Projeto finalizado

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Header = styled.header`
+  background-color: #182c61;
+  color: white;
+  padding: 16px;
+`
+
+const Cabecalho = () => (
+  <Header>
+    <h1>EBAC Jobs</h1>
+  </Header>
+)
+
+export default Cabecalho
